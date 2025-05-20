5b2c628f0bd7f8fce2fef9de2109fcd352537837
Uzair Mohammed <umohammed@redev.co.uk>
2025-05-20 11:56:00 +0100

    added .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_URL = 'http://localhost:5113/api';
+const API_URL = import.meta.env.VITE_API_URL as string;
 
 const api = axios.create({
     baseURL: API_URL,
